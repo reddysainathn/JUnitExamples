d485e5aa9beccc0bf15a278bb45da1571c0fc532
Sainath Reddy <sainathreddynarahari6@gmail.com>
2017-10-10 22:29:27 -0500

    Unit Testing Exceptions

--- src/test/java/ArrayCompareTest.java
+++ src/test/java/ArrayCompareTest.java
@@ -6,12 +6,21 @@ import org.junit.Test;
 
 public class ArrayCompareTest {
 
-	//Array.Sort
-	
+	// Array.Sort
+
 	@Test
 	public void testArraySort_RandomArray() {
-		int[] numbers = {12,3,4,1};
-		int[] expected = {1,3,4,12};
+		int[] numbers = { 12, 3, 4, 1 };
+		int[] expected = { 1, 3, 4, 12 };
+		Arrays.sort(numbers);
+		assertArrayEquals(expected, numbers);
+	}
+
+	// Unit Testing Exceptions.
+	@Test(expected = NullPointerException.class)
+	public void testArraySort_NullArray() {
+		int[] numbers = null;
+		int[] expected = { 1, 3, 4, 12 };
 		Arrays.sort(numbers);
 		assertArrayEquals(expected, numbers);
 	}
