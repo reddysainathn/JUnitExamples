147ca74cd0ef78bb1a7ebb6426efac643883e2de
Sainath Reddy <sainathreddynarahari6@gmail.com>
2017-10-10 22:34:47 -0500

    JUnit performace Test

--- src/test/java/ArrayCompareTest.java
+++ src/test/java/ArrayCompareTest.java
@@ -25,4 +25,14 @@ public class ArrayCompareTest {
 		assertArrayEquals(expected, numbers);
 	}
 
+	// Test to check Array Sort is performing well. (Performance Test).
+	@Test(timeout = 100)
+	public void testSort_Performance() {
+		int array[] = { 12, 23, 4 };
+		for (int i = 0; i <= 1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
+
 }
