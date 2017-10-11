078541cbeac879c0e9c07b1fed3de0fc5ec8b086
Sainath Reddy <sainathreddynarahari6@gmail.com>
2017-10-10 21:50:26 -0500

    Adding JUnit test for another method.

--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -1,16 +1,34 @@
-import static org.junit.Assert.assertEquals;
 import com.example.helper.*;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class StringHelperTest {
-	//Sample JUnit Test
+
+	StringHelper stringHelper = new StringHelper();
+
+	// Sample JUnit Test
 	@Test
-	public void test() {
-		StringHelper stringHelper = new StringHelper();
-		String actual = stringHelper.truncateAInFirst2Positions("AACD");
-		String expected = "CD";
-		assertEquals(expected, actual);
-	
+	public void truncateAInFirst2PositionsTest_2() {
+		assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+	}
+
+	@Test
+	public void truncateAInFirst2PositionsTest_1() {
+		assertEquals("CDA", stringHelper.truncateAInFirst2Positions("ACDA"));
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_1() {
+		assertEquals(false, stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+		assertFalse("Condition Failed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_2() {
+		assertEquals(true, stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+		assertTrue("Condition Passed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
 	}
 
 }
