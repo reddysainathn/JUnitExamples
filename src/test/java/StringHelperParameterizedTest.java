3d49cb9c4b01ceaca3e5ae417b1ab2eb2eb4722e
Sainath Reddy <sainathreddynarahari6@gmail.com>
2017-10-10 22:48:04 -0500

    Parameterized JUnit Testing.

---
+++ src/test/java/StringHelperParameterizedTest.java
@@ -0,0 +1,45 @@
+import com.example.helper.*;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringHelperParameterizedTest {
+
+	StringHelper stringHelper;
+	
+	@Before
+	public void setup() {
+		System.out.println("Executing");
+		stringHelper = new StringHelper();
+	}
+	// Sample JUnit Test
+	@Test
+	public void truncateAInFirst2PositionsTest_2() {
+		assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+	}
+
+	@Test
+	public void truncateAInFirst2PositionsTest_1() {
+		assertEquals("CDA", stringHelper.truncateAInFirst2Positions("ACDA"));
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_1() {
+		assertEquals(false, stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+		assertFalse("Condition Failed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+	}
+
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_2() {
+		assertEquals(true, stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+		assertTrue("Condition Passed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+	}
+	@After
+	public void teardown() {
+		System.out.println("After Test.");
+	}
+
+}
--- src/test/java/StringHelperTest.java
+++ src/test/java/StringHelperTest.java
@@ -2,44 +2,41 @@ import com.example.helper.*;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+// Should declare @RunWith(Parameterized.class) to make it parameterized JUnit test.
+
+@RunWith(Parameterized.class)
 public class StringHelperTest {
 
-	StringHelper stringHelper;
+	StringHelper stringHelper = new StringHelper();
+	private String input;
+	private String expectedOutput; 
 	
-	@Before
-	public void setup() {
-		System.out.println("Executing");
-		stringHelper = new StringHelper();
+	public StringHelperTest(String input, String expectedOutput) {
+		super();
+		this.input = input;
+		this.expectedOutput = expectedOutput;
 	}
-	// Sample JUnit Test
-	@Test
-	public void truncateAInFirst2PositionsTest_2() {
-		assertEquals("CD", stringHelper.truncateAInFirst2Positions("AACD"));
+
+	// Test Conditions are placed here and it is static which returns a Collection.
+	@Parameters
+	public static Collection<String[]> testConditions() {
+		String[][] expectedOutputs = { { "AACD", "CD" }, { "ACD", "CD" } };
+		return Arrays.asList(expectedOutputs);
 	}
 
 	@Test
 	public void truncateAInFirst2PositionsTest_1() {
-		assertEquals("CDA", stringHelper.truncateAInFirst2Positions("ACDA"));
-	}
-
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_1() {
-		assertEquals(false, stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
-		assertFalse("Condition Failed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABCD"));
-	}
-
-	@Test
-	public void testAreFirstAndLastTwoCharactersTheSame_2() {
-		assertEquals(true, stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
-		assertTrue("Condition Passed", stringHelper.areFirstAndLastTwoCharactersTheSame("ABAB"));
-	}
-	@After
-	public void teardown() {
-		System.out.println("After Test.");
+		assertEquals(expectedOutput, stringHelper.truncateAInFirst2Positions(input));
 	}
 
 }
